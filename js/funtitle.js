5db751e9a3273205afabf212c0120240b7f9f9f8
abillgates <vipzxchh@126.com>
2019-04-27 11:34:32 +0800

    Site updated: 2019-04-27 11:34:32

---
+++ js/funtitle.js
@@ -0,0 +1,17 @@
+<!--搞怪提示-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         document.title = '╭(°A°`)╮ 哼,不看我 ~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '(ฅ>ω<*ฅ) 噫表白~' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
